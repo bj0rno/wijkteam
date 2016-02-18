565c4aaa82f2767d6b0cacbd2ed6885a83867578
bjorn <bjornoo1@hotmail.com>
2016-02-18 11:28:41 +0100

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => str_random(10).'@gmail.com'
+        ]);
+    }
+}
